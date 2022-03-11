2c58611c3f1a267a7685d61223b00243537582e6
Ben Holmes <7benholmes@gmail.com>
2022-03-10 18:34:48 -0800

    Added some test files for the lexer

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
-pub fn echo(s: &str){
-    println!("{}", s);
+/// Parsed token.
+/// Stores the kind of token parsed as well as the line number of the program
+pub struct Token {
+    pub kind: TokenKind,
+    pub line: usize,
+}
+
+/// Enum for the lexeme types.
+pub enum TokenKind {
+
 }
